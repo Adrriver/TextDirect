e1b584255be857a10b21e535191879fc68daa8b3
adrriver <arive2@uis.edu>
2017-03-07 17:33:39 -0600

    SessionService begun; AppComponent progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from './session.service';
+import { Router } from '@angular/router';
+import {Subscription} from "rxjs";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    title = 'Public Landing Page';
+    public logInOutcome: string;
+    public username: string;
+    public password: string;
+
+    constructor(private sessionService: SessionService, private router: Router) { }
+    
+    ngOnInit() {
+
+        this.sessionService.logOut();
+        this.logInOutcome = "Welcome!";
+    }
+
+    public logIn(): void {
+
+        if (this.sessionService.logIn(this.username, this.password)) {
+
+            this.logInOutcome = this.username + " " + this.password;
+            
+            this.router.navigate(['main-area/main-home']);
+            
+
+        } else {
+
+            this.logInOutcome = "Please check your credentials\n and try again.\n Thanks!";
+
+        }
+
+    }
+
 }
--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,20 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+//local imports
+import { UserAccount } from './main-area/user-account-settings/user-account';
 
 @Injectable()
 export class SessionService {
 
-  constructor() { }
+    public user: Observable<UserAccount>;
+    public logged: boolean;
+    public url: string;
+    public token: string;
+     
+    constructor(private http: Http) {
+        //clarify URL object use
+        this.logged = false;
+        this.token = 'text_direct_token';
+    }
 
-  public isLoggedIn(): boolean {
+    public logIn(username: string, password: string): boolean {
+        let response;
+        //this.http.get(/*uri/args here */)
+          //  .map(response => { localStorage.setItem(this.token, response.json().data.token); response = true; })
+            //.catch(error => { console.log(error); response = false; });
 
-      return false; //implement authentication logic
+        return true;
 
-  }
+    }
 
-  public isAdmin(): boolean {
+    public logOut(): boolean {
 
-      return false; //implement status logic
+        this.logged = false;
+        this.user = null;
+        localStorage.removeItem(this.token);
 
-  }
+        return true;
+    }
+
+
+    public isLoggedIn(): boolean {
+
+        return true; //implement authentication logic
+
+    }
+
+    public isAdmin(): boolean {
+
+        return false; //implement status logic
+
+    }
+
+    
 
 }
