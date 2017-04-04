b05427d25e1a20b8678853c6537927f69439dce6
adrriver <arive2@uis.edu>
2017-04-04 10:03:19 -0500

    registration - first increment - close to done

---
+++ src/app/form-feedback.directive.ts
@@ -0,0 +1,42 @@
+import {
+  Directive, ElementRef, Input, HostListener, IterableDiffers, IterableDiffer
+} from '@angular/core';
+
+
+@Directive({
+  selector: '[appFormFeedback]'
+})
+export class FormFeedbackDirective  {
+
+  @Input()
+  public cList;
+  public differ: IterableDiffer;
+
+  constructor(private el: ElementRef, private differs: IterableDiffers) {
+
+    this.cList = this.el.nativeElement.classList;
+    this.differ = this.differs.find([]).create(null);
+
+  }
+
+
+  @HostListener('input')
+  check() {
+
+      const warn = this.cList.contains('ng-dirty') && this.cList.contains('ng-invalid');
+      const matHint = this.el.nativeElement.querySelector('div.mat-hint');
+    // const changes = (this.differ.diff(this.cList) as Array<String>).filter( item => item.contains('items.push(item.currentValue.toString()));
+    if (warn) {
+      if (matHint) {
+        matHint.style.color = '#f44336';
+      }
+    } else {
+        if (matHint) {
+          matHint.style.color = '#5e5e5e';
+        }
+
+      }
+
+    }
+
+}
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,41 +7,42 @@ import { CreditFormComponent } from './credit-form/credit-form.component';
 import { Subscription } from 'rxjs/Subscription';
 import { UserAccount } from '../main-area/user-account-settings/user-account';
 
-//services
+// services
 import { SessionService } from '../session.service';
 import { ChildServiceService } from '../child-service.service';
+import {FormFeedbackDirective} from "../form-feedback.directive";
 
 @Component({
     selector: 'app-registration',
     templateUrl: './registration.component.html',
-    styleUrls: ['./registration.component.css'],
-    
+    styleUrls: ['./registration.component.css']
+
 })
 export class RegistrationComponent implements OnInit {
 
-    private regForm: FormGroup;
-    protected shippingAddressSub: FormGroup;  
-    protected pmtArray: FormArray;
-    private paymentOptions: [{}];
-    private childFormGroup: FormGroup;
-    protected childSubscription: Subscription;
-    protected isChildValid: boolean = false;
-    private user: UserAccount;    
-    protected mustCorrect: boolean;
+    public regForm: FormGroup;
+    public shippingAddressSub: FormGroup;
+    public pmtArray: FormArray;
+    public paymentOptions: [{}];
+    public childFormGroup: FormGroup;
+    public childSubscription: Subscription;
+    public isChildValid = false;
+    public user: UserAccount;
+    public mustCorrect: boolean;
 
-                                                    
+
     constructor(private formBuilder: FormBuilder, private sessionService: SessionService,
                 private childService: ChildServiceService) {
                     this.childService.getChildForm().subscribe(form => { this.childFormGroup = form });
                     this.childService.getMessage().subscribe(message => { this.isChildValid = message });
-                    this.sessionService.getUser().subscribe(user => { this.user = user });
-        
+                    this.sessionService.getUser().subscribe(user => { this.user = user; });
+
     }
 
     ngOnInit() {
 
 
-        //this.regForm = new FormGroup({});
+        // this.regForm = new FormGroup({});
 
         this.paymentOptions = [
             { value: 'default', viewValue: 'Select' },
@@ -50,7 +51,7 @@ export class RegistrationComponent implements OnInit {
             { value: 'check', viewValue: 'Checking Account' }
         ];
 
-        //define nested FormGroups before insertion
+        // define nested FormGroups before insertion
         this.shippingAddressSub = new FormGroup({
 
             firstName: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[\\w\\s]+')])),
@@ -71,7 +72,8 @@ export class RegistrationComponent implements OnInit {
             emailAddress: new FormControl('', Validators.compose([Validators.required, CustomValidators.email])),
             firstName: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z- ]+')])),
             lastName: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z- ]+')])),
-            telephoneNumber: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[0-9]{3}-[0-9]{3}-[0-9]{4}')])),
+            telephoneNumber: new FormControl('', Validators.compose([Validators.required,
+                                                 Validators.pattern('[0-9]{3}-[0-9]{3}-[0-9]{4}')])),
             shippingAddress: this.shippingAddressSub,
             pmtMethod: new FormControl(''),
             standing: new FormControl(true),
@@ -84,7 +86,7 @@ export class RegistrationComponent implements OnInit {
             this.regForm.controls['password'].setValue(this.user.password);
             this.regForm.controls['emailAddress'].setValue(this.user.emailAddress);
             this.regForm.controls['firstName'].setValue(this.user.firstName);
-            this.regForm.contains['lastName'].setValue(this.user.lastName);
+            this.regForm.controls['lastName'].setValue(this.user.lastName);
             this.regForm.controls['telephoneNumber'].setValue(this.user.telephoneNumber);
 
             this.shippingAddressSub.controls['firstName'].setValue(this.user.shippingAddress.firstName);
@@ -97,12 +99,12 @@ export class RegistrationComponent implements OnInit {
 
         }
 
-  }   
+  }
 
-         
-    public onSubmit(registrationFormValid, paymentFormValid): void {
-        let success = this.sessionService.createUser(this.regForm, this.childFormGroup);
-        if (!success) this.mustCorrect = true;
+
+    public onSubmit(): void {
+        const success = this.sessionService.createUser(this.regForm, this.childFormGroup);
+        if (!success) { this.mustCorrect = true; }
     }
 
 
